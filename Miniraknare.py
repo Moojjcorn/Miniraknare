a62f4cdb65bfff2683f1744c4ffd9b9402ebbe2f
Moojjcorn <45688077+Moojjcorn@users.noreply.github.com>
2020-11-30 17:03:03 +0100

    Update Miniraknare.py

--- Miniraknare.py
+++ Miniraknare.py
@@ -1,28 +1,24 @@
 while True: 
     print('\n[1] Räkna med + \n[2] Räkna med - \n[3] Räkna med * \n[4] Räkna med / \n[5] Avsluta')
-    choise = int(input('Välj funktion 1 till 5: '))
-    if choise == 1:
-        tal1 = float(input('Skriv ett tal: '))
-        tal2 = float(input('Skriv ett andra tal: '))
-        print('{} + {} = '.format(tal1, tal2))
-        print(tal1 + tal2)
-    elif choise == 2:
-        tal1 = float(input('Skriv ett tal: '))
-        tal2 = float(input('Skriv ett andra tal: '))
-        print('{} - {} = '.format(tal1, tal2))
-        print(tal1 - tal2)
-    elif choise == 3:
-        tal1 = float(input('Skriv ett tal: '))
-        tal2 = float(input('Skriv ett andra tal: '))
-        print('{} * {} = '.format(tal1, tal2))
-        print(tal1 * tal2)
-    elif choise == 4:
-        tal1 = float(input('Skriv ett tal: '))
-        tal2 = float(input('Skriv ett andra tal: '))
-        print('{} / {} = '.format(tal1, tal2))
-        print(tal1 / tal2)
-    elif choise == 5:
-        print('Stänger ned!')
+    choise = input('Välj funktion 1 till 5: ')
+    if choise in ('1', '2', '3', '4'):
+
+      tal1 = float(input('Skriv ett tal: '))
+      tal2 = float(input('Skriv ett andra tal: '))
+      if choise == '1':
+          print('{} + {} = '.format(tal1, tal2))
+          print(tal1 + tal2)
+      elif choise == '2':
+          print('{} - {} = '.format(tal1, tal2))
+          print(tal1 - tal2)
+      elif choise == '3':
+          print('{} * {} = '.format(tal1, tal2))
+          print(tal1 * tal2)
+      elif choise == '4':
+          print('{} / {} = '.format(tal1, tal2))
+          print(tal1 / tal2)
+    elif choise == '5':
+        print('Avslutar...')
         break
     else:
-        print('Ogiltig funktion!')
+        print('Ogiltig funktion')
