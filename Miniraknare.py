2f07a7939c7c7f966c08e019f9991125604bb4f7
Moojjcorn <45688077+Moojjcorn@users.noreply.github.com>
2020-11-30 14:52:10 +0100

    Update Miniraknare.py

--- Miniraknare.py
+++ Miniraknare.py
@@ -1,9 +1,28 @@
 while True: 
-    print('[1] Räkna med + \n[2] Räkna med - \n[3] Räkna med * \n[4] Räkna med / \n[5] Avsluta')
-    choise = (input('Välj funktion 1 till 5: '))
-    if choise in ('1', '2', '3', '4'):
+    print('\n[1] Räkna med + \n[2] Räkna med - \n[3] Räkna med * \n[4] Räkna med / \n[5] Avsluta')
+    choise = int(input('Välj funktion 1 till 5: '))
+    if choise == 1:
         tal1 = float(input('Skriv ett tal: '))
         tal2 = float(input('Skriv ett andra tal: '))
+        print('{} + {} = '.format(tal1, tal2))
+        print(tal1 + tal2)
+    elif choise == 2:
+        tal1 = float(input('Skriv ett tal: '))
+        tal2 = float(input('Skriv ett andra tal: '))
+        print('{} - {} = '.format(tal1, tal2))
+        print(tal1 - tal2)
+    elif choise == 3:
+        tal1 = float(input('Skriv ett tal: '))
+        tal2 = float(input('Skriv ett andra tal: '))
+        print('{} * {} = '.format(tal1, tal2))
+        print(tal1 * tal2)
+    elif choise == 4:
+        tal1 = float(input('Skriv ett tal: '))
+        tal2 = float(input('Skriv ett andra tal: '))
+        print('{} / {} = '.format(tal1, tal2))
+        print(tal1 / tal2)
     elif choise == 5:
-        print('Stänger ned')
+        print('Stänger ned!')
         break
+    else:
+        print('Ogiltig funktion!')
